bc2c5ec343415afcf7b250c4d13db36f798e60f2
Jerry <fenturechance@gmail.com>
2020-03-19 14:56:41 +0800

    List

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Page from './Page'
+import List from './List'
 
 function App() {
+  let numbers = [1, 2, 3, 4, 5]
   return (
     <div className="App">
-        <Page></Page>
+        <List numbers={numbers}></List>
     </div>
   );
 }
